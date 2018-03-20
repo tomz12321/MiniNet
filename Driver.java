bdbe34a47c0e95eb781b4cac8c581bc0551689be
tomz123 <tomz12321@gmail.com>
2018-03-20 20:17:19 +1100

    Create Driver Class
    Create Driver Class

---
+++ Driver.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+/**
+ * Write a description of class Driver here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Driver
+{
+    // instance variables - replace the example below with your own
+    private ArrayList<People> peopleList;
+
+    /**
+     * Constructor for objects of class Driver
+     */
+    public Driver()
+    {
+        //initialise the variables
+        peopleList = new ArrayList<People>();
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+    
+    //Method ! Add a person into the network -> add()
+    
+    //! Select a person by name -> select()
+    
+    //! Display the profile of the selected person -> display()
+    
+    //! Update the profile information of the selected person -> update()
+    
+    //! Delete the selected person -> delete()
+    
+    //! Connect two persons in a meaningful way e.g. friend, parent -> connect()
+    
+    //! Find out whether a person is a direct friend of another person ->
+    
+    //! Find out the name(s) of a person’s child(ren) or the names of the parents
+}
